e8e9c1867eebf8d2e1c25e6e806dbf63aa5e3ea0
Ye Yu <rafolwen98@gmail.com>
2020-10-13 14:37:30 +0800

    Use flex for header

--- frontend/src/components/commons/header.js
+++ frontend/src/components/commons/header.js
@@ -45,40 +45,48 @@ export default class Header extends React.Component {
   }
 
   renderMore() {
-    let brand = <div className="ml-4 mt-1 py-2 px-3 navbar-brand flex-column justify-content-center">
+    let brand = <div className="ml-4 mt-1 py-2 px-0 px-lg-3 navbar-brand flex-column justify-content-center h-100">
+      <div className="d-flex flex-column justify-content-center h-100">
         <div className="d-flex justify-content-center">
           <img src={process.env.PUBLIC_URL + '/ticketer.png'} className="logo my-auto" />
-          <span className="pl-2 pr-1 my-auto h6 d-none d-lg-block">
+          <span className="pl-2 pr-1 my-auto h6">
               Ticketer ≫ <b>{this.props.section}</b>
           </span>
         </div>
+      </div>
     </div>;
 
-    let searchField = <form className="mr-4 mt-1 py-2 flex-grow-0 flex-md-grow-1 flex-column justify-content-center">
-      <div>
+    let searchField = <form className="d-none d-lg-block mr-4 mt-1 py-2 flex-fill flex-column justify-content-center h-100">
+      <div className="d-flex flex-column justify-content-center h-100">
         <input className="form-control form-control-sm rounded-pill px-3" type="text" placeholder="Search ticket by title or number" />
       </div>
     </form>;
 
-    let loginLink = <div className="pr-1 mt-1 mr-3 flex-column justify-content-center">
+    let loginLink = <div className="pr-1 mt-1 ml-auto mr-3 flex-column justify-content-center h-100">
+      <div className="d-flex flex-column justify-content-center h-100">
         <div className="d-flex justify-content-center">
           <Link to="/login">
             <span className="h6 font-weight-bold my-0 py-0">Log In</span>
           </Link>
         </div>
+      </div>
     </div>;
 
-    let logoutLink = <div className="pr-1 mt-1 mr-3 flex-column justify-content-center">
+    let spacer = <div className="h-100 border-left" style={{width: "30px"}}>&nbsp;</div>;
+
+    let registerLink = <div className="pr-1 mt-1 mr-3 flex-column justify-content-center h-100">
+      <div className="d-flex flex-column justify-content-center h-100">
         <div className="d-flex justify-content-center">
           <Link to="/register">
             <u className="h6 font-weight-bold my-0 py-0">Register</u>
           </Link>
         </div>
+      </div>
     </div>;
 
-    return <nav className="navbar border-bottom border-header shadow-sm d-flex flex-row">
-      {brand} {searchField} {loginLink} {logoutLink}
-    </nav>;
+    return <div className="border-bottom border-header shadow-sm d-flex flex-row" style={{height: "70px"}}>
+      {brand} {spacer} {searchField} {loginLink} {registerLink}
+    </div>;
   }
 
   render() {
