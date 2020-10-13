66ee596000987b17494e5e9675fbdfc78a8fee52
Ye Yu <rafolwen98@gmail.com>
2020-10-13 15:47:54 +0800

    Animate login form collapse

--- frontend/src/components/commons/header.js
+++ frontend/src/components/commons/header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import "./header.css";
 import { Link } from "react-router-dom";
-
+import Collapse from "react-bootstrap/Collapse";
 export default class Header extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {profile: null};
+    this.state = {
+      profile: null,
+      showLoginForm: false
+    };
   }
 
   componentDidMount() {
@@ -24,6 +27,10 @@ export default class Header extends React.Component {
     this.setState({
       profile: "profile"
     });
+  }
+
+  setShowLoginForm(show) {
+    this.setState({showLoginForm: show});
   }
 
   renderSimple() {
@@ -56,21 +63,24 @@ export default class Header extends React.Component {
 
     let searchField = <input className="form-control form-control-sm rounded-pill px-3" type="text" placeholder="Search ticket by title or number" />;
 
-    let loginLink = <div className="ml-auto mr-0 my-0 px-2 px-lg-3 h-100 d-flex flex-column justify-content-center h-100">
+    const showLoginForm = this.state.showLoginForm;
+    let loginLink = <a href="#login-form" className="ml-auto mr-0 my-0 px-2 px-lg-3 h-100 d-flex flex-column justify-content-center h-100" onClick={() => this.setShowLoginForm(!showLoginForm)} aria-controls="login-form" aria-expanded={showLoginForm} >
       <div className="d-flex justify-content-center">
-        <Link to="/login">
-          <span className="h6 font-weight-bold my-0 py-0">Log In</span>
-        </Link>
+        <span className="h6 font-weight-bold my-0 py-0">Log In</span>
       </div>
-    </div>;
+    </a>;
 
-    let registerLink = <div className="mx-0 my-0 pl-2 pr-3 pl-lg-3 pr-lg-5 h-100 d-flex flex-column justify-content-center h-100">
-      <div className="d-flex justify-content-center">
-        <Link to="/register">
-          <u className="h6 font-weight-bold my-0 py-0">Register</u>
-        </Link>
+    let loginForm = <Collapse in={showLoginForm}>
+      <div id="login-form">Login Form goes here</div>
+    </Collapse>;
+
+    let registerLink = <Link to="/register">
+      <div className="mx-0 my-0 pl-2 pr-3 pl-lg-3 pr-lg-5 h-100 d-flex flex-column justify-content-center h-100">
+        <div className="d-flex justify-content-center">
+            <u className="h6 font-weight-bold my-0 py-0">Register</u>
+        </div>
       </div>
-    </div>;
+    </Link>;
 
     return <div>
       <div className="border-bottom border-header shadow-sm d-flex flex-row" style={{height: "70px"}}>
@@ -82,6 +92,7 @@ export default class Header extends React.Component {
         </div>
         {loginLink} {registerLink}
       </div>
+      {loginForm}
       <div className="d-md-none d-block w-100 px-5 pt-3">
         <div> {searchField} </div>
       </div>
