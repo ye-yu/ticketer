24a70e6512c82b0e62bf24ebebda0bd6366ac412
Ye Yu <rafolwen98@gmail.com>
2020-10-14 23:36:27 +0800

    Add feed description

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -7,6 +7,23 @@ import {
 } from "react-router-dom";
 import Header from "./commons/header";
 
+const topic = {};
+topic.hot = <small>
+  <b>Hot ticket</b> is for showing tickets that has been gaining too many attention
+  due to their their vote count and fork activity. Get on the queue and investigate
+  why did they become the center of attention this week!
+</small>;
+topic.top = <small>
+  <b>Top community</b> is for showing the communities that has been active since the
+  past week. An active community means a community that has a lot of hot tickets, forked
+  tickets, and ticket updates.
+</small>;
+topic.controversial = <small>
+  <b>Bad reputation</b> is for showing tickets that did not perform well on the platform.
+  This may include tickets that has low votes, has negative impact on the community, etc.
+  Go ahead and study what does the community consider as a bad ticket.
+</small>;
+
 class ActivityBadge extends React.Component {
   render() {
     if (this.props.linkTo) return <a href={this.props.linkTo} className={this.props.classNameExtra + " d-inline-block border px-3 py-2 small"}>
@@ -25,17 +42,24 @@ export default class Home extends React.Component {
       <div className="container mt-md-5 mt-3">
         <div className="row">
           <div className="col px-4">
-            <div className="d-flex flex-column flex-md-row px-3 bg-white border bg-white border rounded-lg">
-              <div className="h4 mr-md-auto text-center py-3 my-0 d-none d-md-inline ml-3">Activity</div>
-              <div className="d-flex d-md-inline justify-content-center py-3">
-                {this.props.posts == "hot" ? <ActivityBadge classNameExtra="hover-bg-border-danger rounded-left" title="Hot Ticket" /> :  <ActivityBadge linkTo="/" classNameExtra="hover-with-border-danger rounded-left" title="Hot Ticket" />}
-                {this.props.posts == "top" ? <ActivityBadge classNameExtra="hover-bg-border-primary rounded-0 border-left-0 border-right-0" title="Top Community" /> :  <ActivityBadge linkTo="/top" classNameExtra="hover-with-border-primary rounded-0 border-left-0 border-right-0" title="Top Community" />}
-                {this.props.posts == "controversial" ? <ActivityBadge classNameExtra="hover-bg-border-warning rounded-right" title="Bad Reputation" /> :  <ActivityBadge linkTo="/controversial" classNameExtra="hover-with-border-warning rounded-right" title="Bad Reputation" />}
-
+            <div className="border bg-white border rounded-lg">
+              <div className="d-flex flex-column flex-md-row px-3">
+                <div className="h4 mr-md-auto text-center py-3 my-0 d-none d-md-inline ml-3">Activity</div>
+                <div className="d-flex d-md-inline justify-content-center py-3">
+                  {this.props.posts == "hot" ? <ActivityBadge classNameExtra="hover-bg-border-danger rounded-left" title="Hot Ticket" /> :  <ActivityBadge linkTo="/" classNameExtra="hover-with-border-danger rounded-left" title="Hot Ticket" />}
+                  {this.props.posts == "top" ? <ActivityBadge classNameExtra="hover-bg-border-primary rounded-0 border-left-0 border-right-0" title="Top Community" /> :  <ActivityBadge linkTo="/top" classNameExtra="hover-with-border-primary rounded-0 border-left-0 border-right-0" title="Top Community" />}
+                  {this.props.posts == "controversial" ? <ActivityBadge classNameExtra="hover-bg-border-warning rounded-right" title="Bad Reputation" /> :  <ActivityBadge linkTo="/controversial" classNameExtra="hover-with-border-warning rounded-right" title="Bad Reputation" />}
+                </div>
               </div>
+            </div>
+            <div className="mt-3 px-3 py-3 border bg-white border rounded-lg d-block d-lg-none">
+              {topic[this.props.posts]}
             </div>
           </div>
           <div className="col-3 d-none d-lg-block">
+            <div className="mb-3 px-3 py-3 d-none d-lg-block bg-white border rounded-lg d-flex flex-column text-justify" style={{lineHeight:"1.15rem"}}>
+              {topic[this.props.posts]}
+            </div>
             <div className="bg-white border rounded-lg d-flex flex-column">
               <div className="h4 border-bottom py-3 text-center bg-light shadow-sm">What do I do?</div>
               <div href="#" className="py-1 px-3 small">
